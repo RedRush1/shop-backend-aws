7043e5bb071b48ebf2d75724de983108c1cf8f92
Vasyl Rebresh <rebresh58@gmail.com>
2026-04-25 13:18:21 +0300

    feat-4: example

--- bin/shop-backend-aws.ts
+++ bin/shop-backend-aws.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
 import { ProductServiceStack } from "../lib/product-service/product-service-stack";
+import {TodoStack} from "../lib/todo/TodoStack";
 
 const app = new cdk.App();
 
 new ProductServiceStack(app, 'ProductServiceStack', {});
+new TodoStack(app, 'TodoStack');
